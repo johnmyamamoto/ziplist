5e8e67b73636b5e1ae89747437963ec2ce4c0502
johnmyamamoto <142952678+johnmyamamoto@users.noreply.github.com>
2024-09-22 23:46:12 -1000

    commit

---
+++ ziplist.ts
@@ -0,0 +1,23 @@
+function zipList(list1: unknown[], list2: unknown[]): unknown[] {
+  const newList: unknown[] = [];
+  for (let i = 0; i < list1.length; i++) {
+    newList.push(list1[i]);
+    newList.push(list2[i]);
+  }
+
+  return newList;
+}
+
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+
+function zipListTheFunctionalWay(list1: unknown[], list2: unknown[]): unknown[] {
+  let i = 0;
+  list1.map((item) => {
+    list2.push(list1[i]);
+    i++;
+    return item;
+  });
+  return list2;
+}
+
+console.log(zipListTheFunctionalWay(['a', 'b', 'c'], [1, 2, 3]));
